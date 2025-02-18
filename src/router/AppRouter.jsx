7d38b2b2b381d0ecf1cc016a3adee2cfe4e45604
Sebastián Guzmán <20200981@aloe.ulima.edu.pe>
2025-02-18 18:03:19 -0500

    creacion rutas

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar/pages/CalendarPage";
+
+export const AppRouter = () => {
+    const authStatus = 'non-authenticated'
+    return (
+        <Routes>
+            {
+                (authStatus === 'non-authenticated')
+                ? <Route path="/auth/*" element={<LoginPage/>}/>
+                : <Route path="/*" element={<CalendarPage/>}/>
+            }
+
+            <Route path="/*" element={<Navigate to={"/auth/login"}/>}/>
+        </Routes>
+    );
+};
